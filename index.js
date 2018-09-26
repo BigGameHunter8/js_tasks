8b8c0ca85425b5e575f5d19d6b932fad333e265c
Dmitri Zabiaco <Dmitri.Zabiaco@endava.com>
2018-09-26 10:56:16 +0300

    first task

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function each(arr,callback,thisArgs){
-    
-}
+const binaryArrayToNumber = arr => {
+    // your code
+    let str = arr.toString();
+    let n = str.replace(/,/g,"");
+    var digit = parseInt(n,2);
+    console.log(digit);
+  };
+
+  binaryArrayToNumber([0,1,1,1]);
